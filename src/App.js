d57a48eb376369370a5df0a11ed92b292f118f93
hildakim <pinkredjohn@gmail.com>
2022-05-03 12:50:33 +0900

    init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import PostDetail from './pages/PostDetail';
+import 'antd/dist/antd.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CreatePost from './pages/CreatePost';
+import UpdatePost from './pages/UpdatePost';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/post/:id" element={<PostDetail />}>
+        </Route>
+        <Route path="/" element={<Home />}>
+        </Route>
+        <Route path="/create" element={<CreatePost />}>
+        </Route>
+        <Route path="/post/:id/update" element={<UpdatePost />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
